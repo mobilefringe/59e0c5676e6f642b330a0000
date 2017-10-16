8fbfc891e75d8216b4abafa32c43e41d43e513b4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-16 18:01:16 -0400

    Hazeldean Mall 2.0 1.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -183,7 +183,7 @@ function init_map(reg){
         colors: {stroke: '#cccccc', selected: -20, hover: "#cccccc"},
         height:800,
         width:1140,
-        viewBox:[1000,420,1650,1650],
+        viewBox:[10020,420,1650,1650],
         regions: reg,
         tooltipsMode:'custom',
         zoom: true,
