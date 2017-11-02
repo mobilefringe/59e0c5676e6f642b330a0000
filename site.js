9f4455d1b247d10223ed24677822e62f6753cd67
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-02 13:56:44 -0400

    Hazeldean Mall 2.0 1.3  /site.js

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var site_json = {
                     "mall_name" : "Yuba Sutter Mall",
-                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/59e0c5676e6f642b330a0000/image/png/1507920555000/logohazel.png",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/59e0c5676e6f642b330a0000/image/png/1509634558000/HM Black on White PNG (1).png",
                     "time_zone" : "US/Eastern",
                     // "newsletter_form_url" : "//mobilefringe.createsend.com/t/d/s/bkhoy/?callback=?",
                     // "newsletter_email_field" : "cm-bkhoy-bkhoy"
