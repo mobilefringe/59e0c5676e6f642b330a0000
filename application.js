0e3136ba4d330f38ffdaa050608a8684ebfb5453
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-03 12:12:49 -0400

    Hazeldean Mall 2.0 1.3  /application.js

--- application.js
+++ application.js
@@ -77,6 +77,9 @@ function renderPromotions(container, template, collection){
         } else {
             val.store_name = site_json.mall_name;
             val.image_url = site_json.default_image;
+            if(val.promo_image_url_abs.indexOf('missing.png') > 0){ 
+                val.promo_image_url_abs = site_json.default_image;
+            }
         }
         console.log(val);
         if(val.image_url.indexOf('missing.png') > 0){ 
