ecf9eb8b5894b2c740f0195ae3573160abf3f275
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-06 17:16:19 -0500

    Hazeldean Mall 2.0 1.3  /application.js

--- application.js
+++ application.js
@@ -558,6 +558,7 @@ function renderHours(container, template, collection, type){
         collection = item_list;
     }
     if (type == "reg_hours") {
+        var sunday_val ;
         $.each( collection , function( key, val ) {
             if (!val.store_id && val.is_holiday == false) {
                 switch(val.day_of_week) {
