af1e033088d327200902183a00f02fc3a33d0b07
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-07 15:42:09 -0500

    Hazeldean Mall 2.0 1.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -60,6 +60,7 @@ $(window).load(function(e){
         $('#site_search_m').val('');
         $('#site_search').val('');
         $('.close_search_m').hide();
+        $('.close_search_desk').hide();
         
     })
     $('.close_search_desk').click(function(){
