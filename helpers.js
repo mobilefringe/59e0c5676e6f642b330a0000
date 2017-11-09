3182910d18b66e9d4ea65b8a560d5508b3158b5d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-09 15:35:49 -0500

    Hazeldean Mall 2.0 1.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -109,6 +109,32 @@ function init(e){
 function show_content(){
     $('.yield').fadeIn();
     $(".modal-backdrop").remove();
+    
+    var d = new Date();
+    var n = d.getDay();
+    var today_hours = getTodaysHours();
+    renderHomeHours('.home_hours_container', '.home_hours_template', today_hours)
+    renderHomeHours('#footer_hours_container', '#footer_hours_template', today_hours)
+    $.each( getPropertyHours(), function(i,v){
+        if(v.is_closed == true){
+            var hours_day = new Date(v.holiday_date + "T05:00:00Z")
+            if(hours_day.setHours(0, 0, 0, 0) == d.setHours(0, 0, 0, 0)){
+                $('.hours_today').text("Closed Today")
+            }
+        }
+        if(v.is_holiday == true){
+            var hours_day = new Date(v.holiday_date + "T05:00:00Z")
+            if(hours_day.setHours(0, 0, 0, 0) == d.setHours(0, 0, 0, 0)){
+                console.log(v)
+                var open_time = new Date (v.open_time);
+                var close_time = new Date (v.close_time);
+                v.open_time = convert_hour(open_time);
+                v.close_time = convert_hour(close_time);
+                v.h = v.open_time+ " - " + v.close_time;
+                $('#hours_home').text(v.h)
+            }
+        }
+    });
 }
 
 
